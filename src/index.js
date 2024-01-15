b82e5a1d50243d699c900080dcb5b941fef5d29a
gssp4167 <gssp4167@gmail.com>
2024-01-15 23:10:33 +0530

    Completed consumption and sending mail

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 const express=require('express');
+const amqplib=require('amqplib');
+const {EmailService} = require('./services');
+
+async function connectQueue(){
+    try {
+        const connection=await amqplib.connect("amqp://localhost");
+        const channel = await connection.createChannel();
+        await channel.assertQueue("noti-queue");
+        channel.consume("noti-queue",async (data)=>{
+            // console.log(`${Buffer.from(data.content)}`);
+            const object=JSON.parse(`${Buffer.from(data.content)}`);
+            await EmailService.sendEmail("emailnotifierproject@gmail.com",object.recepientEmail,object.subject,object.text);
+            channel.ack(data);
+        });
+    } catch (error) {
+        
+    }
+}
+
 
 const {ServerConfig,Logger}=require('./config');
 const apiRoutes=require('./routes');
@@ -11,7 +30,9 @@ app.use(express.urlencoded({extended:true}));
 
 app.use('/api',apiRoutes);
 
-app.listen(ServerConfig.PORT,()=>{
+app.listen(ServerConfig.PORT,async ()=>{
     console.log(`Successfully started the server on PORT: ${ServerConfig.PORT}`);
+    await connectQueue();
+    console.log("Queue is up");
     // Logger.info("Successfully started the server",{msg:"success"});
 });
