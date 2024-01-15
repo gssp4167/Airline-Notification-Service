c163ff10115b1bec430e1f8f354a81eabfde31e4
gssp4167 <gssp4167@gmail.com>
2024-01-15 11:41:21 +0530

    Created nodemailer transport object and tried

--- src/index.js
+++ src/index.js
@@ -2,11 +2,27 @@ const express=require('express');
 
 const {ServerConfig,Logger}=require('./config');
 const apiRoutes=require('./routes');
+
+const mailsender=require('./config/email-config');
+
 const app=express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.use('/api',apiRoutes);
 
-app.listen(ServerConfig.PORT,()=>{
+app.listen(ServerConfig.PORT,async ()=>{
     console.log(`Successfully started the server on PORT: ${ServerConfig.PORT}`);
-    Logger.info("Successfully started the server",{msg:"success"});
+    // Logger.info("Successfully started the server",{msg:"success"});
+    try {
+        const response=await mailsender.sendMail({
+            from:ServerConfig.GMAIL_EMAIL,
+            to:'abc@gmail.com',
+            subject:'Testing email api',
+            text:'Email API working fine, sent by Gautham'
+        });
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
 });
